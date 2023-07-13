7c0d7b3c2dd6c1b761d4e5ceb5392f061f2415d1
luulinhvu29 <luulinhvu29@gmail.com>
2023-07-13 14:48:20 +0700

    as

--- nienluan/app/Http/Controllers/Api/GoogleController.php
+++ nienluan/app/Http/Controllers/Api/GoogleController.php
@@ -33,6 +33,7 @@ class GoogleController extends Controller
     {
         $user = Socialite::driver('google')->user();
 
+        dd($user);
         // Xử lý thông tin của người dùng và đăng nhập thành công
         $user = User::where('email', $user->email)->first();
 
@@ -62,51 +63,4 @@ class GoogleController extends Controller
         }
     }
 
-//    public function getGoogleSignInUrl()
-//    {
-//        try {
-//            $url = Socialite::driver('google')->stateless()
-//                ->redirect()->getTargetUrl();
-//            return response()->json([
-//                'url' => $url,
-//            ])->setStatusCode(Response::HTTP_OK);
-//        } catch (\Exception $exception) {
-//            return $exception;
-//        }
-//    }
-//
-//    public function loginCallback(Request $request)
-//    {
-//        try {
-//            $state = $request->input('state');
-//
-//            parse_str($state, $result);
-//            $googleUser = Socialite::driver('google')->stateless()->user();
-//
-//            $user = User::where('email', $googleUser->email)->first();
-//            if ($user) {
-//                throw new \Exception(__('google sign in email existed'));
-//            }
-//            $user = User::create(
-//                [
-//                    'email' => $googleUser->email,
-//                    'name' => $googleUser->name,
-//                    'google_id'=> $googleUser->id,
-//                    'password'=> '123',
-//                    'level' =>Constant::user_level_client,
-//                ]
-//            );
-//            return response()->json([
-//                'status' => __('google sign in successful'),
-//                'data' => $user,
-//            ], Response::HTTP_CREATED);
-//
-//        } catch (\Exception $exception) {
-//            return response()->json([
-//                'status' => __('google sign in failed'),
-//                'error' => $exception,
-//                'message' => $exception->getMessage()
-//            ], Response::HTTP_BAD_REQUEST);
-//        }
-//    }
 }
